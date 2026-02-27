de5690706e8223e315d580c657dc642b0645f6d7
datingwithme2007-glitch <datingwithme2007@gmail.com>
2026-02-27 09:06:57 +0000

    puri update of filrs

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('PrideApp Signaling Server is running');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('signal', (data) => {
+    socket.broadcast.emit('signal', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
